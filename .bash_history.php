92aa9ee848f6f476d8f8ff41d03767103e532ba9
imambz08 <imambz08@gmail.com>
2021-02-23 15:12:00 +0700

    0.0.01

---
+++ .bash_history.php
@@ -0,0 +1,44 @@
+cd /var/www/html/Keylist/application/
+ls
+cd views/
+ls
+cd...
+cd
+cd /var/www/html/Keylist/application/
+ls
+cd /config/
+cd config/
+ls
+sudo nano database.php
+sudo nano database.php
+ls
+cd ../
+ls
+cd /var/www/html/Keylist/application/
+ls
+cd /controllers
+cd /controllers/
+cd controllers/
+ls
+nano C_Utama.php
+nano C_Utama.php
+cd
+sudo su
+cd /var/www/html/Keylist/application/
+ls
+cd views/
+ls
+touch home.php
+ls
+ls -1
+chmod 777 home.php
+ls -1
+sudo nano home.php
+ls -1
+sudo nano home.php
+ls -1
+exit
+
+
+
+
